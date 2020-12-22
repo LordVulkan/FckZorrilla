dd15af771f0c4f093b1c66b95142be8321a80a5f
LordVulkan <jomai92@gmail.com>
2020-12-22 14:22:41 +0100

    Initial Commit

--- src/meta.js
+++ src/meta.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name        Fuck Mickel Zorrilla 
-// @namespace   Espinof
-// @description Save Espinof from Mickel Zorrilla.
+// @namespace   https://github.com/LordVulkan/FckZorrilla
+// @description Save Espinof from Mikel Zorrilla.
 // @match       *://*.espinof.com/*
 // @grant       none
 // @version     process.env.VERSION
-// @author      -
+// @author      jomai92@gmail.com
 // @require     https://cdn.jsdelivr.net/combine/npm/@violentmonkey/dom@1,npm/@violentmonkey/ui@0.4
+// @downloadURL https://github.com/LordVulkan/FckZorrilla/releases/latest/download/FckZorrilla.js
 // ==/UserScript==
