956a21150b8328390e25a9dd9a40c80c07a660af
LordVulkan <jomai92@gmail.com>
2020-12-22 15:40:14 +0100

    Improve metadata

--- src/meta.js
+++ src/meta.js
@@ -6,6 +6,9 @@
 // @grant       none
 // @version     process.env.VERSION
 // @author      jomai92@gmail.com
+// @license     GPL-3.0-only
 // @require     https://cdn.jsdelivr.net/combine/npm/@violentmonkey/dom@1,npm/@violentmonkey/ui@0.4
 // @downloadURL https://github.com/LordVulkan/FckZorrilla/releases/latest/download/FckZorrilla.js
+// @homepageURL https://github.com/LordVulkan/FckZorrilla
+// @copyright 2020, LordVulkan (https://github.com/LordVulkan)
 // ==/UserScript==
